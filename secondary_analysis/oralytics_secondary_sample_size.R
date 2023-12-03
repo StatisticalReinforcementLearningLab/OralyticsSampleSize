e4f154d32c596746c0d42b8f30a62eb8b750d55f
kellywzhang <kellywzhang@hotmail.com>
2023-12-03 00:13:35 -0500

    small update to secondary analysis code

--- secondary_analysis/oralytics_secondary_sample_size.R
+++ secondary_analysis/oralytics_secondary_sample_size.R
@@ -5,7 +5,7 @@ library(MRTSampleSizeBinary)
 
 BPstart <- 0.15 # BP stands for Base Probabilities under no prompt
 BPend <- 0.1
-RRstart <- 1.35
+RRstart <- 1.4
 RRend <- 1.25
 
 
@@ -34,6 +34,7 @@ plot(all_days, TE_vals, xlab="Day in Study", ylab="Relative Risk",
 decision_times <- 70*2
 avail_pattern <- rep(1, decision_times)
 lower_probs <- rep(1, decision_times)*0.2
+upper_probs <- rep(1, decision_times)*0.8
 
 null_exp_full <- log((BPstart / BPend)) / log(140)
 null_feats <- matrix(data = c(rep(1, decision_times), 
@@ -53,7 +54,7 @@ plot(exp(TE_feats %*% TE_coef)) # sanity check
 sample_size <- mrt_binary_ss(avail_pattern=avail_pattern, 
                              f_t=TE_feats, g_t=null_feats, 
                              beta=TE_coef, alpha=null_coef,
-                             p_t=lower_probs,    # randomization probability
+                             p_t=upper_probs,    # randomization probability
                              gamma=0.05,         # type 1 error
                              b=.2,               # power of 0.8
                              exact=FALSE)        # exact means rounds to nearest whole number
